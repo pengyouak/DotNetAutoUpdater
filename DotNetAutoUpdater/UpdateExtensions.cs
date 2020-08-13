6c05248b5ef9618b93977935c4ec4441a65cb6ce
付金龙 <fujinlong@bjknrt.com>
2020-08-13 14:30:52 +0800

    增加扩展方法

---
+++ DotNetAutoUpdater/UpdateExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace DotNetAutoUpdater
+{
+    public static class UpdateExtensions
+    {
+        public static void UpdateUI(this Control control, MethodInvoker methodInvoker)
+        {
+            if (control.InvokeRequired)
+            {
+                while (!control.IsHandleCreated)
+                {
+                    if (control.Disposing || control.IsDisposed)
+                        return;
+                }
+                control.Invoke(methodInvoker);
+            }
+            else
+                methodInvoker();
+        }
+    }
+}
