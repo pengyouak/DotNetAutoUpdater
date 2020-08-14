5ca8dfc29cb04728b01e74f92db9d98443a1f27b
付金龙 <fujinlong@bjknrt.com>
2020-08-14 14:04:42 +0800

    增加静态方法，增加服务器地址属性，调整json为xml

--- DotNetAutoUpdater/UpdateOption.cs
+++ DotNetAutoUpdater/UpdateOption.cs
@@ -1,18 +1,21 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
 
 namespace DotNetAutoUpdater
 {
+    [Serializable]
     public class UpdateOption
     {
-        [JsonIgnore]
+        [XmlIgnore]
         public bool IsUpdateAvailable { get; set; }
 
-        [JsonIgnore]
+        [XmlIgnore]
         public Version InstalledVersion { get; set; }
 
-        [JsonIgnore]
+        [XmlIgnore]
         public Version UpdateVersion { get; private set; }
 
         private string _version = "";
@@ -36,10 +39,35 @@ namespace DotNetAutoUpdater
             }
         }
 
+        public string ServerUrl { get; set; }
+
         public UpdateMode UpdateMode { get; set; }
 
         public string ChangeLog { get; set; }
 
         public List<UpdateItem> UpdateItems { get; set; }
+
+        public static UpdateOption LoadUpdateOption(string xml)
+        {
+            XmlSerializer xs = XmlSerializerHelper.Create(typeof(UpdateOption));
+
+            // XmlSerializer xs = XmlSerializer.FromTypes(new Type[] { typeof(UpdateOption) }).FirstOrDefault();
+
+            // XmlSerializer xs = new XmlSerializer(typeof(UpdateOption));
+
+            var sr = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            var config = xs.Deserialize(sr) as UpdateOption;
+            sr.Close();
+
+            return config;
+        }
+
+        public static void SaveUpdateOption(UpdateOption updateOption, string file)
+        {
+            XmlSerializer xs = XmlSerializerHelper.Create(typeof(UpdateOption));
+            StreamWriter sw = new StreamWriter(file);
+            xs.Serialize(sw, updateOption);
+            sw.Close();
+        }
     }
 }
