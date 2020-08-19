a809f9e8fe9bdfc2eec573762215f5257b8fdcdf
付金龙 <fujinlong@bjknrt.com>
2020-08-19 09:34:44 +0800

    优化

--- DotNetAutoUpdater/UpdateMode.cs
+++ DotNetAutoUpdater/UpdateMode.cs
@@ -1,20 +1,25 @@
-﻿namespace DotNetAutoUpdater
+﻿using System.Xml.Serialization;
+
+namespace DotNetAutoUpdater
 {
     public enum UpdateMode
     {
         /// <summary>
         /// 提示
         /// </summary>
+        [XmlEnum("0")]
         Prompt,
 
         /// <summary>
         /// 提示并显示更新通知
         /// </summary>
+        [XmlEnum("1")]
         PromptAndDetail,
 
         /// <summary>
         /// 强制更新
         /// </summary>
+        [XmlEnum("2")]
         Force
     }
 }
